38b71177e7894867df84bfde7428f06d30a28474
Arkajit Majumder <arkajeet.majumder@gmail.com>
2021-02-14 19:52:07 +0530

    commited 4

--- app.py
+++ app.py
@@ -9,11 +9,14 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        search_image = request.form['image_name'].replace(' ', '')
-        number_of_image = int(request.form['number'])
-        image_scrapper(image_name=search_image, number_of_image=number_of_image)
-        image_names = os.listdir('./static/images/store_image/'+search_image)
-        return render_template('index.html', folder=search_image, pics=image_names)
+        try:
+            search_image = request.form['image_name'].replace(' ', '')
+            number_of_image = int(request.form['number'])
+            image_scrapper(image_name=search_image, number_of_image=number_of_image)
+            image_names = os.listdir('./static/images/store_image/'+search_image)
+            return render_template('index.html', folder=search_image, pics=image_names)
+        except:
+            return render_template('index.html')
     else:
 
         return render_template('index.html')
